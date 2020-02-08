7bbd3ee6d9cee7c4fade9900ce27891007ac2583
Greg Keuken <gkeuken@ca.ibm.com>
2020-02-08 06:19:48 -0600

    add build to zhw.sh test

--- tests/zhw.sh
+++ tests/zhw.sh
@@ -5,6 +5,8 @@ mydir=$(callerdir ${0})
 
 # First, make sure the zhw repo has been installed
 
+${mydir}/../../zbrew/build.sh # required if first ever run
+
 ZHW110DIR="${mydir}/../../zbrew-zhw/zhw110/"
 if ! [ -e "${ZHW110DIR}" ]; then
         echo "Need to install zhw repo to run this test" >&2
