6b04740518daed19ae24871315ea285e37e9d0ad
Greg Keuken <gkeuken@ca.ibm.com>
2020-02-08 05:58:14 -0600

    fix zhw.sh and add smpappacc

--- tests/zhw.sh
+++ tests/zhw.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-/cbuild/zbrew-zhw/tests/zhwoverride.sh
+
+. zbrewfuncs
+mydir=$(callerdir ${0})
+
+# First, make sure the zhw repo has been installed
+
+ZHW110DIR="${mydir}/../../zbrew-zhw/zhw110/"
+if ! [ -e "${ZHW110DIR}" ]; then
+        echo "Need to install zhw repo to run this test" >&2
+        exit 1
+fi
+../../zbrew-zhw/tests/zhwoverride.sh
+
