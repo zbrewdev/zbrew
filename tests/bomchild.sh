e058bb9a352d3ba4d0d93f22864c044d8adf4079
Greg Keuken <gkeuken@ca.ibm.com>
2020-03-05 09:58:56 -0500

    tests for bom parent and child reads

---
+++ tests/bomchild.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Basic test to ensure Parent bomread working ok
+#
+. zbrewfuncs
+mydir=$(callerdir ${0})
+#set -x
+
+# First, make sure the zhw repo has been installed
+
+zbrew_dir="${mydir}/../../zbrew"
+if ! [ -e "${zbrew_dir}" ]; then
+	echo "Need to install zbrew to run this test" >&2
+	exit 1
+fi
+
+
+actual=`readchild bomtest <${zbrew_dir}/tests/bomtest.json`
+zbrewtest "Bill of Materials (BoM) file parsing failed" "0" "$?"
+
+expected=" CHILD1 CHILD2 CHILD3"
+zbrewtest "Unexpected datasets" "${expected}" "${actual}"
+
+
+exit 0
