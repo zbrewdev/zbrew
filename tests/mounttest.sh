cbfbc530eb35691237859bd0bbeddb6e7ab21b62
Tom Zhang <tom.yczhang@gmail.com>
2020-06-12 18:47:45 -0400

    more updates

--- tests/mounttest.sh
+++ tests/mounttest.sh
@@ -30,8 +30,8 @@ zzz
 }
 
 function cleanup {
-    drm -f "${ZBREW_SRC_HLQ}ZHW110*.**"
-    drm -f "${ZBREW_TGT_HLQ}ZHW110*.**"
+    drm -f "${ZBREW_SRC_HLQ}$swname*.**"
+    drm -f "${ZBREW_TGT_HLQ}$swname*.**"
     rm $err 2>/dev/null
 }
 
@@ -46,49 +46,54 @@ function assert {
 #main
 . zbrewsetenv
 
-export ZBREW_SRC_HLQ=ZBREWTS.
+export ZBREW_SRC_HLQ=ZBREWMS.
 export ZBREW_SRC_ZFSROOT=/zbrew/mts/
-export ZBREW_TGT_HLQ=ZBREWTT.
+export ZBREW_TGT_HLQ=ZBREWMT.
 export ZBREW_TGT_ZFSROOT=/zbrew/mtt/
+
+unset swname hfs1 hfs2
+swname=ZHW110
+hfs1=SZHWHFS
+hfs2=SZHWHFS2
 
 err="${ZBREW_TMP}/err.out"
 cleanup
 
-crtzfs ${ZBREW_SRC_HLQ}ZHW110.SZHWHFS
-crtzfs ${ZBREW_SRC_HLQ}ZHW110.SZHWHFS2
+crtzfs ${ZBREW_SRC_HLQ}$swname.$hfs1
+crtzfs ${ZBREW_SRC_HLQ}$swname.$hfs2
 touch "${err}"
 
 #test1: a valid case
 #-------------------------------------------------
-zbrewmount -m -s ZHW110 2>$err
+zbrewmount -m -s $swname 2>$err
 rc1=$?
 assert 0 $rc1
-zbrewtest "mount ZHW110 at source" "0" "$rc1"
-zbrewmount -u -s ZHW110 2>$err
+zbrewtest "mount $swname at source" "0" "$rc1"
+zbrewmount -u -s $swname 2>$err
 rc2=$?
 assert 0 $rc2
-zbrewtest "unmount ZHW110 at source" "0" "$rc2"
+zbrewtest "unmount $swname at source" "0" "$rc2"
 
 #test2: trying to mount target which ZFS DS does not exist yet
 #-------------------------------------------------
-zbrewmount -m -t ZHW110 2>$err
+zbrewmount -m -t $swname 2>$err
 rc3=$?
 assert 2 $rc3
-zbrewtest "mount ZHW110 at target" "2" "$rc3"
+zbrewtest "mount $swname at target" "2" "$rc3"
 
 #test3: now should be valid
 #-------------------------------------------------
-crtzfs ${ZBREW_TGT_HLQ}ZHW110.SZHWHFS
-crtzfs ${ZBREW_TGT_HLQ}ZHW110.SZHWHFS2
+crtzfs ${ZBREW_TGT_HLQ}$swname.$hfs1
+crtzfs ${ZBREW_TGT_HLQ}$swname.$hfs2
 
-zbrewmount -m -t ZHW110 2>$err
+zbrewmount -m -t $swname 2>$err
 rc4=$?
 assert 0 $rc4
-zbrewtest "mount ZHW110 at target" "0" "$rc4"
-zbrewmount -u -t ZHW110 2>$err
+zbrewtest "mount $swname at target" "0" "$rc4"
+zbrewmount -u -t $swname 2>$err
 rc5=$?
 assert 0 $rc5
-zbrewtest "unmount ZHW110 at target" "0" "$rc5"
+zbrewtest "unmount $swname at target" "0" "$rc5"
 
 #cleanup
 cleanup
